830aaf110c5c266992221b0926927ab1d27326ab
John Krueger <jtmkrueger@gmail.com>
2013-08-09 13:31:25 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Watg::Application.config.secret_key_base = '2d1d92c917d8f0e2039bb7c74bbab1eff377f9914cda68043cba42a5c578882e030eff210af2fb10464586c9f882ea013f4640f343c5da072617cbaef35a4740'
